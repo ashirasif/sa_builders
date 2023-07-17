0b36f285044bb452b0e76fb5bec72b5219f69e2f
Muhammad Ashir <mashir670@gmail.com>
2023-07-17 22:08:05 +0500

    tickered with r3f a bit

--- sa_builders/src/App.jsx
+++ sa_builders/src/App.jsx
@@ -1,29 +1,25 @@
-import { useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import './App.css'
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
-    <div id="canvasContainer">
-		<Canvas>
-			<ambientLight intensity={0.1} />
-			<directionalLight color="red" position={[0, 0, 5]} />
-			<mesh>
-				<boxGeometry />	
-				<meshStandardMaterial />
-			</mesh>
-		</Canvas>
-	</div>
-  )
+    <div id="canvasContainer" style={{ width: "100%", height: "100%" }}>
+      <Canvas>
+        <color attach="background" args={["black"]} />
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
+createRoot(document.getElementById("root")).render(<App />);
 
-
-
-createRoot(document.getElementById("root")).render(<App/>)
-
-export default App
-
+export default App;
