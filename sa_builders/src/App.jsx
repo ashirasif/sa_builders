8316183cf53c21aa17c1f4c15ab3c253520cf6ce
Muhammad Ashir <mashir670@gmail.com>
2023-07-26 22:02:09 +0500

    navbar edit

--- sa_builders/src/App.jsx
+++ sa_builders/src/App.jsx
@@ -1,9 +1,16 @@
-import { useRef, useState, Suspense, lazy, useEffect, useLayoutEffect} from "react";
+import {
+  useRef,
+  useState,
+  Suspense,
+  lazy,
+  useEffect,
+  useLayoutEffect,
+} from "react";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import { OrbitControls } from "@react-three/drei";
 
-// components import 
+// components import
 import SAloading from "./sa_3d/loading";
 import NavBar from "./dom_comps/nav_bar";
 // const Tester = lazy(() => delayForDemo(import("./testing_helpers/testers")));
@@ -16,26 +23,33 @@ import NavBar from "./dom_comps/nav_bar";
 
 function App() {
   return (
-    <div id="canvasContainer" style={{position:"fixed",top:0,left:0,width:"100vw",height:"100vh"}}>
-      <Canvas camera={{ position: [0,0,15] }} >
+    <div
+      id="canvasContainer"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Canvas camera={{ position: [0, 0, 15] }}>
         <color attach="background" args={["black"]} />
-        <Suspense fallback={<SAloading />} > 
-        
-        </Suspense>
-      </Canvas>      
-      <div className="fixed top-0 left-3 container w-screen">
-      <NavBar cont={{
-        PROJECT:"",
-        "BUY/SELL PROPERTY":"",
-        ARCHITECTURE:"",
-        FURNITURE:"",
-        CONTACT: ""
-      }}/>
+        <Suspense fallback={<SAloading />}></Suspense>
+      </Canvas>
+      <div className="fixed top-0 left-3 w-screen">
+        <NavBar
+          cont={{
+            PROJECT: "",
+            "BUY/SELL PROPERTY": "",
+            ARCHITECTURE: "",
+            FURNITURE: "",
+            CONTACT: "",
+          }}
+        />
       </div>
     </div>
   );
 }
-
-
 
 export default App;
--- sa_builders/src/dom_comps/nav_bar.jsx
+++ sa_builders/src/dom_comps/nav_bar.jsx
@@ -1,20 +1,24 @@
+import logo_img from "../../public/logo_2.jpg";
 
-
-function NavBar({cont}) {
-
-         
-    return (
-        <div className="nav-bar w-full container flex flex-row text-white justify-evenly gap-5">
-        
-            {Object.keys(cont).map(key => 
-              
-                <a href={cont[key]} key={key} className="basis-auto">{key}</a>
-
-            )}        
-
-        </div>
-    )
+function Logo() {
+  return (
+    <div className="basis-12">
+      <img className="aspect-auto" src={logo_img} />
+    </div>
+  );
 }
 
+function NavBar({ cont }) {
+  return (
+    <div className="nav-bar w-full flex flex-row text-white justify-evenly gap-5 lg:block">
+      <Logo />
+      {Object.keys(cont).map((key) => (
+        <a href={cont[key]} key={key} className="basis-auto">
+          {key}
+        </a>
+      ))}
+    </div>
+  );
+}
 
-export default NavBar
+export default NavBar;
